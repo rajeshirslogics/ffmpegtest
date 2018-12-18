ef02c6f685c14287ed8d1bde000a96dad2d1480a
R.Rajesh Swamy <rajesh@logics.com>
2018-12-18 17:34:58 +0530

    command updates

--- ffmpegcommand.js
+++ ffmpegcommand.js
@@ -8,11 +8,13 @@ executecommand = (command) => {
 
 streamcam = (device) => {
   setTimeout(function () {
-    executecommand(`ffmpeg -r 30 -i /dev/video${device} -r 6 -vcodec libx264 -tune zerolatency -b 600k -f mpegts udp://localhost:123${device}`);
+    let strcommand = `ffmpeg -r 30 -i /dev/video${device} -r 6 -vcodec libx264 -tune zerolatency -b 600k -f mpegts udp://localhost:123${device}`;
+    console.log(strcommand);
+    executecommand(strcommand);
   }, (10 * 1000));
 }
 
 for (let index = 0; index < 2; index++) {
-  streamcam(index);  
+  streamcam(index);
 }
 //executecommand('ffmpeg -i /dev/video0 -vcodec libx264 -tune zerolatency -b 1200k -f mpegts udp://localhost:1234');
